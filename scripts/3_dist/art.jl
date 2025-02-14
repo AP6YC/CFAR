054bf031d57751b3e3e79e3e9f33e138f1912a44
Sasha Petrenko <sap625@umsystem.edu>
2025-02-13 22:33:53 -0600

    Comments

--- scripts/3_dist/art.jl
+++ scripts/3_dist/art.jl
@@ -14,9 +14,9 @@ CFAR.run_exp(config_file="art-dist.yml")
 
 # CFAR.run_exp(config_file="cvi-dist.yml")
 
-pargs = CFAR.dist_exp_parse("CFAR experiment.")
-opts = CFAR.load_opts("art-dist.yml", pargs)
-sim_params, varying = CFAR.config_to_params(opts, pargs)
+# pargs = CFAR.dist_exp_parse("CFAR experiment.")
+# opts = CFAR.load_opts("art-dist.yml", pargs)
+# sim_params, varying = CFAR.config_to_params(opts, pargs)
 
 
 
